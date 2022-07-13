0220a04318a43c7e7bfbf0f0a83f64dee07d1033
joseluissantanabautista <2104Duna>
2022-07-13 15:12:42 +0200

    code 2.0

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -7,15 +7,33 @@ public class QueryProcessor {
 
     public String process(String query) {
 		query = query.toLowerCase();
+        // String query = "what is 12 plus 15"
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Jose";
-        }
-            // please dont remove anything
-            return "";
+        } else if (query.contains("plus")){
+            // Here we get a list of words
+            String[] words = query.split(" ");
+            int sum = 0;
+            for (String word : words){
+                // see if its a word or a number
+                try{
+                    int number = Integer.parseInt(word);
+                    sum += number;
+                } catch(Exception e){
+                    //
+                }
 
+
+            }
+           return String.valueOf(sum);
+        } else if (query.contains("largest")){
+
+        }
+            // please dont remove anythingg
+            return "";
     }
 }
