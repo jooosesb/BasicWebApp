6bf5693fb85fe1d3676305fa69894f6e01a40bbb
joseluissantanabautista <2104Duna>
2022-07-13 14:35:32 +0200

    git

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -14,6 +14,7 @@ public class QueryProcessor {
         } else if (query.contains("name")) {
            return "MyTeam";
         } else { // TODO extend the programm here
+            // please dont remove anything
             return "";
         }
     }
